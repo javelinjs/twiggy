3ef59870ee481cfbd8c74b4357c54157b4e81ed5
Yizhi Liu <javelinjs@gmail.com>
2015-04-12 20:15:00 +0800

    abstract customer

--- src/main/scala/me/yzhi/twiggy/system/Customer.scala
+++ src/main/scala/me/yzhi/twiggy/system/Customer.scala
@@ -4,9 +4,22 @@ import me.yzhi.twiggy.system.Node.NodeID
 
 /**
  */
-class Customer(val name: String) {
+abstract class Customer(val name: String) {
   val sys = Postoffice.instance
   val exec = new Executor(this)
+
+  // A customer must have an unique name in order to communicate with the
+  // customer with the same name but at a different machine.  It will inherent
+  // its parent's (if not empty) node information, for example, the logisitic
+  // regression application and its weight are parent and child,
+  // respectively. They should have the same nodes information.
+  //Customer(const string& my_name, const string& parent_name = "");
+  //virtual ~Customer();
+
+  // process a message received from a remote node. It will be called by
+  // executor's processing thread
+  def process(msg: Message): Unit
+  def slice(msg: Message, krs: Vector[me.yzhi.twiggy.util.Range]): Vector[Message]
 
   def port(nodeId: NodeID): RemoteNode = {
     val rnode = exec.rnode(nodeId)
